7e01ce3f66c9d121d7af99a3a5e3410a2d764dc2
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-07-13 23:28:12 +0300

    dz

--- public/index.php
+++ public/index.php
@@ -2,22 +2,8 @@
 
 include '../vendor/autoload.php';
 
-// use TexLab\MyDB\DbEntity;
-// use TexLab\MyDB\DB;
-// use Core\Config;
-// use TexLab\MyDB\Runner;
 use Model\Model;
 
-// $table = new Runner(DB::Link([
-//       'host' => Config::MYSQL_HOST,
-//       'username' => Config::MYSQL_USER_NAME,
-//       'password' => Config::MYSQL_PASSWORD,
-//       'dbname' => Config::MYSQL_DATABASE
-//   ]));
-//   // print_r($table->runSQL("SELECT * FROM `group`,`users` WHERE `group`.`id` = `users`.`id_cod`"));
-//   print_r($table->runSQL("SELECT `group`. `cod` FROM `group`,`users` WHERE `group`.`id` = `users`.`id_cod` AND `login` = 'Anna' AND `password` = '123'"));
-$model = new Model();
-print_r($model->checkUser('Anna','123'));
 ?>
 
  <!doctype html>
@@ -32,14 +18,16 @@ print_r($model->checkUser('Anna','123'));
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
    </head>
    <body>
-   <form class="m-3 w-25">
+
+  <h1> <?= !empty($_POST) && !empty((new Model())->checkUser($_POST['Login'], $_POST['Password'])) ? 'Welcome!' : 'Такого пользователя нет' ?></h1>
+   <form class="m-3 w-25" method="post" action="?">
   <div class="form-group ">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="text" name="Login" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" name="Password" class="form-control" id="exampleInputPassword1">
   </div>
   
   <button type="submit" class="btn btn-primary">Submit</button>
--- src/Core/Config.php
+++ src/Core/Config.php
@@ -6,7 +6,7 @@ class Config
 {
     const MYSQL_HOST = 'localhost';
     const MYSQL_USER_NAME = 'root';
-    const MYSQL_PASSWORD = '';
+    const MYSQL_PASSWORD = 'root';
     const MYSQL_DATABASE = 'newff';
     const PAGE_SIZE = 3;
     const MYSQL_TABLE = 'users';
